22a9db5847ff3bb5fcaaf87cd800dc160c429066
Mehmet Gökkaya <mehmet@mehmetgokkaya.com>
2017-12-30 12:13:46 +0300

    Update sidebar.js

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -2,4 +2,21 @@
    $(function(){var o=$("#sidebar-wrapper").offset(),r=$("#enson").offset().top-$("#sidebar-wrapper").outerHeight()-0;$(window).scroll(function(){$(window).scrollTop()>o.top?$(window).scrollTop()<r&&$("#sidebar-wrapper").stop().animate({marginTop:$(window).scrollTop()-o.top+60}):$("#sidebar-wrapper").stop().animate({marginTop:0})})});
 function goster(){$("div#panel").show(100);}function gizle(){$("div#panel").hide(100);}function gostergizle(){$("div#panel").toggle(100);}
 $(document).ready(function () { $("div#panel").hide(100); });
+;
+;
+var countDownDate = new Date("Jan 1, 2018 00:00:00").getTime();
+var countdownfunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("gerisayim").innerHTML = days + " Gün " + hours + " Saat "
+    + minutes + " Dakika " + seconds + " Saniye ";
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("gerisayim").innerHTML = "Mutlu Yıllar";
+    }
+}, 1000);
 //]]>
