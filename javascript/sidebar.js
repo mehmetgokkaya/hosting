2d03568d32e5d9e149e5383554c793de4c69989b
Mehmet Gökkaya <mehmet@mehmetgokkaya.com>
2017-12-30 11:17:58 +0300

    Update sidebar.js

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,3 +1,5 @@
 //<![CDATA[
    $(function(){var o=$("#sidebar-wrapper").offset(),r=$("#enson").offset().top-$("#sidebar-wrapper").outerHeight()-0;$(window).scroll(function(){$(window).scrollTop()>o.top?$(window).scrollTop()<r&&$("#sidebar-wrapper").stop().animate({marginTop:$(window).scrollTop()-o.top+60}):$("#sidebar-wrapper").stop().animate({marginTop:0})})});
+function goster(){$("div#panel").show(100);}function gizle(){$("div#panel").hide(100);}function gostergizle(){$("div#panel").toggle(100);}
+$(document).ready(function () { $("div#panel").hide(100); });
 //]]>
