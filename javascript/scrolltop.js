351736381513fc9caaa1ca940eb0e147ee245102
Mehmet Gökkaya <mehmet@mehmetgokkaya.com>
2018-02-01 17:02:21 +0300

    Create scrolltop.js

---
+++ javascript/scrolltop.js
@@ -0,0 +1,1 @@
+$(function(){var o=$(&quot;#headerSlideContainer&quot;),t=o.css(&quot;top&quot;);$(window).scroll(function(){$(this).scrollTop()&gt;300?o.stop().animate({top:&quot;0px&quot;},0):o.stop().animate({top:t},0)})});
